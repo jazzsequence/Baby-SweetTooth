c9d3c7d0c4420e814e52a492b87f5932c5fd04d3
Chris Reynolds <chris@jazzsequence.com>
2011-01-13 16:52:08 -0700

    added language_attributes() to header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php include (TEMPLATEPATH.'/get-theme-options.php'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="distribution" content="global" />
